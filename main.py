f8902999c57b63f464485d565389080bfc76d7b9
Zehight <1318141712@qq.com>
2023-03-25 20:03:28 +0800

    欧克

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 #     allMessages.append(answer['response_format'])
 #     print(answer['tokens'],answer['response_format']['content'])
 
+
+
 def chat(question):
     result = pyrequests.post('https://api.miragari.com/fast/fastChat',json={'question':question})
-    return json.loads(result.text)['answer']
+    return json.loads(result.text)
 
 @app.route('/', methods=['POST','GET'])
 def fast_chat():
     if request.method == 'POST':
         question = json.loads(request.data.decode('utf-8'))['question']
-        answer = chat(question)
-        return {'answer':answer}
+        return chat(question)
     if request.method == 'GET':
         question = request.args.get('q')
         if question is None:
             return '欢迎哦~'
         else:
-            answer = chat(question)
-            return answer['response_format']['content']
+            return chat(question)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
