0b3411b6657c34dd2d6477697fd5e10103ce3601
Zehight <1318141712@qq.com>
2023-03-25 12:28:32 +0800

    ok

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-from flask import Flask, jsonify
 import os
-
-import openai
+import requests as pyrequests
 from flask import Flask, request, render_template, jsonify, json
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-
-# 设置OpenAI API key
-openai.api_key = "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"
-
-systemSetting = {'role': 'system', 'content': '你是一个用萌萌的语气说话的助手'}
-allMessages = [systemSetting]
-
-# 基本chat
-def chat(allMessages):  #定义一个函数，以便后面反复调用
-    if type(allMessages)!=list:
-        allMessages = [systemSetting,{'role': 'user', 'content': allMessages}]
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=allMessages,)
-    response_format = {
-        'role': response['choices'][0]['message']['role'],
-        'content': response['choices'][0]['message']['content'],
-    }
-    return {'response_format':response_format,'tokens':response['usage']['total_tokens']}
 
 # 一轮对话
 # def chat_round(ask):
@@ -31,12 +12,16 @@ def chat(allMessages):  #定义一个函数，以便后面反复调用
 #     allMessages.append(answer['response_format'])
 #     print(answer['tokens'],answer['response_format']['content'])
 
+def chat(question):
+    result = pyrequests.post('https://api.miragari.com/fast/fastChat',json={'question':question})
+    return json.loads(result.text)['answer']
+
 @app.route('/fastChat', methods=['POST','GET'])
 def fast_chat():
     if request.method == 'POST':
         question = json.loads(request.data.decode('utf-8'))['question']
         answer = chat(question)
-        return {"answer":answer}
+        return {'answer':answer}
     if request.method == 'GET':
         question = request.args.get('q')
         answer = chat(question)
